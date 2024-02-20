fe84576b6f12185d999063d142aff141b78223e5
Lakshita-Goyal <146127187+Lakshita-Goyal@users.noreply.github.com>
2024-02-20 11:04:17 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,30 @@
+mylist=[1,4,7,"Radha","Shyam","Krishna",20,92,18,16]
+print(mylist[6])
+mylist[0]
+mylist[4]
+
+#append
+list1=["Strwaberry","orange","Apple"]
+list2=["these are fruits"]
+list1.append(list2)
+print(list1)
+
+
+#silicing
+print("list[0:8]=",mylist[0:8])
+print("list[2:5]=",mylist[2:5])
+
+#length of list
+print("length of list=",len(mylist))
+
+#append---add in the last of list
+mylist.append("Lakshita d2")
+mylist 
+
+#reversing
+print(mylist[::-1])
+
+#Pop
+list[0,1,2,3,4,5,6,7]
+L1=[1,2,3,4,5,6,7]
+L1.pop(1)
