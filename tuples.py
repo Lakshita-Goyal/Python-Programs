39bf0be9d57be8e84d5ad3d080f8a18c6867b472
Lakshita-Goyal <146127187+Lakshita-Goyal@users.noreply.github.com>
2024-02-22 10:09:09 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,27 @@
+mytuple=[1,3,56,45]
+print(mytuple[3])
+
+#append
+mytuple1=["ram","shyam"]
+
+mytuple.append(mytuple1)
+print(mytuple1)
+
+
+#slicing
+print("tuple[1:2]=",mytuple[1:2])
+print("tuple[3:4]=",mytuple[3:4])
+
+#length of tuple
+print("length of tuple=",len(mytuple1))
+
+#indexing
+print(mytuple)
+
+#repetition
+repeated_mytuple=mytuple1*2
+print(repeated_mytuple)
+
+#concatenation
+mytuple2=mytuple+mytuple1
+print(mytuple2)
