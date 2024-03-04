c03ee53be30dec935d8afc7f96e2530df196d3cd
Lakshita-Goyal <146127187+Lakshita-Goyal@users.noreply.github.com>
2024-03-04 09:49:21 +0530

    Add files via upload

---
+++ Class&Objects.py
@@ -0,0 +1,85 @@
+# objects and class
+# class A:
+#     age=10
+#     print(age)
+# obj=A()
+
+
+# class A:
+#    def_init_(self):
+#      age=10
+#     print(age)
+# obj=A()
+
+# class A:
+#     """Learn coding with........."""
+#     age=10
+#     print(age)
+# obj=A()
+# print(obj.age)
+# print(A.age)
+# print(obj.__doc__)
+# print(A.__doc__)
+
+# class A:
+#     age=12
+#     """Learn coding with us.........."""
+#     print(age)
+# obj=A()
+# print(obj.age)
+# print(A.age)
+# print(obj.__doc__)
+
+# class A:
+#     age=13
+#     def fun():
+#         name="Learn coding"
+#         print(name)
+# obj=A()
+# print(obj.fun)
+# print(obj.age)
+# print(A.age)
+
+# class A:
+#     age=50
+#     def fun(age):
+#         """code with me............"""
+#         name="self coder"
+#         print(name)
+# obj=A()
+# obj.fun()
+# print(obj.age)
+# print(A.age)
+# print(obj.fun.__doc__)
+
+# class A:
+#     def fun(self,name,age,hello):
+#         print(age," ",name," ",hello)
+# obj=A()
+# obj.fun(20,"AAdil","World")
+# obj.fun(30,"Lakshita","jaipur")
+
+# class A:
+#     age=30
+#     def __init__(self,name,age,address):
+#       print(name," ",age," ",address)
+# obj=A("lakshita",20,"Jaipur")
+
+# class A:
+#     def __init__(self):
+#         name="okay"
+#         print(name)
+# obj=A()
+
+# class A:
+#     def __init__(self,age,name,address):
+#         address="jaipur"
+#         print(age,name,address)
+# obj=A(12,"hello","none")
+
+# class A:
+#     name1="Lakshita"
+#     def __init__(self,age,name,address):
+#         address="jodhpur"
+#         print(age,name,address,self.name1)
+# obj=A(20,"simran",None)
