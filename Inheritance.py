7b82e152eefb0860ef242a7367295b53f530335c
Lakshita-Goyal <146127187+Lakshita-Goyal@users.noreply.github.com>
2024-03-09 12:46:06 +0530

    Add files via upload

---
+++ Inheritance.py
@@ -0,0 +1,65 @@
+# class parent():
+#     def house(self):
+#         print("having the house on 200 acres of land")
+# class child(parent):
+#     def money(self):
+#         print("child has only 10 lakh of money")
+# C=child()
+# C.house()
+# C.money()
+
+
+# class parent():
+#     def house(self):
+#         print("having the house on 200 acres of land")
+# class child(parent):
+#     def money(self):
+#         print("child has only 10 lakh of money")
+# P=parent()
+# P.house()
+# C.money()
+
+
+# class A:
+#     def __init__(self):
+#         self.num1=int(input("enter the number 1:"))
+#         self.num2=int(input("enter the number 2:"))
+#     def Add(self):
+#         print("Addition:",self.num1+self.num2)
+#     def Sub(self):
+#         print("Substraction:",self.num1-self.num2)
+
+# class B(A):
+#     def Mul(self):
+#         print("Mutiplication:",self.num1*self.num2)
+#     def Div(self):
+#         print("Division:",self.num1/self.num2)
+#     def Rem(self):
+#         print("Reminder:",self.num1%self.num2)
+
+# obj= B()
+# obj.Add()
+# obj.Sub()
+# obj.Mul()
+# obj.Div()
+# obj.Rem()
+
+
+# class Muskan():
+#     Back='Oracle database and java'
+#     def backend(self):
+#         print("Backend task implementation",self.Back)
+# class Simran:
+#     Front='jango Html CSS'
+#     def frontend(self):
+#         print("Frontend task implementation",self.Front)
+# class Aadil(Muskan,Simran):
+#     def show(self):
+#         print("Dynamic web creator")
+
+# A=Aadil()
+# A.backend()
+# A.frontend()
+# A.show()
+
+
